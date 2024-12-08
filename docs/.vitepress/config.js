ce2b77e8413f50ffaa25459702ee242b26bcedad
zhangjingguang <2586300277@qq.com>
2024-12-08 09:19:46 +0800

    docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,10 +5,11 @@ export default defineConfig({
   outDir: 'dist',
   title: "Three.js 编辑器",
   link: '/',
-  description: "three-editor-cores基于Three.js, 实现组态化, 高度灵活扩展的低代码, 无代码, 快速开发的三维场景可视化编辑器内核",
+  description: "THREE-EDITOR 是使用 npm 库 three-editor-cores基于Three.js, 实现组态化, 高度灵活扩展的低代码, 无代码, 快速开发的三维场景可视化编辑器内核",
   themeConfig: {
     nav: [
-      { text: '指南', link: '/markdown-examples' },
+      { text: '指南', link: '/createIdea' },
+      { text: '开源案例', link: 'https://z2586300277.github.io/three-cesium-examples/#/example' },
       { text: '使用', link: 'https://z2586300277.github.io/three-editor/dist/#/example' },
       { text: '编辑器', link: 'https://z2586300277.github.io/three-editor/dist/#/editor' },
       { text: 'npm', link: 'https://www.npmjs.com/package/three-editor-cores' }
