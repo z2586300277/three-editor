1174714d813287226a568c2f8660e5cdf80c450d
Pkcha <pkcha@PkchadeMacBook-Air.local>
2026-04-15 20:54:44 +0800

    feat: initial release

--- src/editor/scene.vue
+++ src/editor/scene.vue
@@ -18,6 +18,13 @@ function getEvent(e) {
 
     props.emitEditor.threeEditor.getSceneEvent(e, info => {
 
+        if (!info) {
+            props.emitEditor.info = null
+            props.emitEditor.threeEditor.currentInfo = null
+            props.emitEditor.threeEditor.handler.currentInfo = null
+            return
+        }
+
         props.emitEditor.info = info
 
         if (info.mode === '点击信息') {
@@ -37,6 +44,8 @@ function getEvent(e) {
 }
 
 function createScene(sceneParams) {
+
+    props.emitEditor.info = null
 
     if (!sceneParams) {
 
@@ -116,24 +125,14 @@ function createScene(sceneParams) {
 
     props.emitEditor.threeEditor = threeEditor
 
-    props.emitEditor.info = null
-
-    threeEditor.scene.addEventListener('objectRemoved', () => {
-        if (!props.emitEditor.info) return
-        const { currentModel, currentRootModel } = props.emitEditor.info
-        const inScene = (obj) => obj && threeEditor.scene.getObjectById(obj.id)
-        if (!inScene(currentModel) || !inScene(currentRootModel)) {
-            threeEditor.setOutlinePass([])
-            threeEditor.transformControls.detach()
-            props.emitEditor.info = null
-        }
-    })
-
     window.onresize = () => threeEditor.renderSceneResize()
 
 }
 
-onUnmounted(() => props.emitEditor.threeEditor?.destroySceneRender())
+onUnmounted(() => {
+    props.emitEditor.info = null
+    props.emitEditor.threeEditor?.destroySceneRender?.()
+})
 
 props.emitEditor.createScene = createScene
 
